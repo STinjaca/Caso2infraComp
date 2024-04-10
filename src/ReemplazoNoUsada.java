0108a274d9a5009e4524ae5ac8e5a81dcbb22791
Santi T <s.tinjaca@uniandes.edu.co>
2024-04-09 21:53:31 -0500

    Revert "Merge branch 'prueba'"
    This reverts commit 71b6e94f0e275c52785de5b809b055bfd51d11e7, reversing
    changes made to f2a59b0bcf229217bc5ec62bb008b48e235c9885.

--- src/ReemplazoNoUsada.java
+++ src/ReemplazoNoUsada.java
@@ -1,32 +1,32 @@
 
-public class ReemplazoNoUsada extends Thread {
+public class ReemplazoNoUsada extends Thread{
     private int marcosPagina;
     private int[][] contadores;
-
+    
+    
     // Contructor algoritmo reemplazo menos reciente
-    public ReemplazoNoUsada(int numMarcosP) {
+	public ReemplazoNoUsada(int numMarcosP) {
         this.marcosPagina = numMarcosP;
         this.contadores = new int[numMarcosP][3];
-    }
+	}
 
     // ActualizarContadores
     private void actualizarContadores() {
         synchronized (contadores) {
             for (int i = 0; i < marcosPagina; i++) {
-                contadores[i][0] = 0;
+            	contadores[i][0]=0;
             }
         }
     }
 
     public void marcarReferenciaPagina(int pR, int pV, String pagRef) {
         synchronized (contadores) {
-            contadores[pR][2] = pV;
+        	contadores[pR][2] = pV;
             if (pagRef.equals("R")) {
-                contadores[pR][0] = 1;
-            } else if (pagRef.equals("W")) {
-                contadores[pR][0] = 1;
-                contadores[pR][1] = 1;
-            }
+            	contadores[pR][0] = 1;}
+            else if(pagRef.equals("W")){
+            	contadores[pR][0] = 1;
+                contadores[pR][1] = 1;}	
         }
     }
 
@@ -70,7 +70,7 @@ public class ReemplazoNoUsada extends Thread {
         while (true) {
             actualizarContadores();
             try {
-                Thread.sleep(4); // 4ms
+                Thread.sleep(4); //4ms
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
